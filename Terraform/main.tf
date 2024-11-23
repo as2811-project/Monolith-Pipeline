98b847a39d1adceb41d4b32a570c7353449b46bd
as2811-project <anandh.tedxchennai@gmail.com>
2024-11-23 18:51:49 +1100

    fixed script to install java on launch.

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -12,6 +12,47 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
+resource "aws_s3_bucket" "movie-list-kafka-monolith" {
+  bucket = "movie-list-kafka-monolith"
+}
+
+resource "aws_security_group" "ssh_access" {
+  name = "kafka-ec2-sg"
+  description = "adds appropriate ssh permission"
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+  }
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_iam_role" "glue_role" {
+  name = "glue_job_role"
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "*",
+            "Resource": "*"
+        },
+    ]
+})
+}
+
 resource "aws_instance" "stream_server" {
   ami           = "ami-06d2149e11dd4bec4"
   instance_type = "t2.micro"
@@ -19,5 +60,6 @@ resource "aws_instance" "stream_server" {
     Name = var.instance_name
   }
   user_data = file("scripts/start_script.sh")
-  security_groups = ["default"]
+  security_groups = [aws_security_group.ssh_access.name]
+  key_name = "KafkaKeyPair"
 }
--- Terraform/scripts/start_script.sh
+++ Terraform/scripts/start_script.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+sudo yum update -y
+cd /home/ec2-user
 wget https://downloads.apache.org/kafka/3.9.0/kafka_2.12-3.9.0.tgz
-tar -xvf kafka_2.12-3.3.1.tgz
-yum install java-1.8.0-openjdk
+tar -xvf kafka_2.12-3.9.0.tgz
+sudo yum install java-1.8.0-amazon-corretto-devel -y
